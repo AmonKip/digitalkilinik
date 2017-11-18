25f5c552d7e80a9adf4bc23a30bc2e3d169f3948
KIPROTICH <kiprotich87@yahoo.com>
2017-11-17 20:50:32 -0500

    comment out logging classes

--- Logging/DBLoggerProvider.cs
+++ Logging/DBLoggerProvider.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace ePatientCare.Logging
 {
-      public class DBLoggerProvider : ILoggerProvider
-      {
-          private readonly Func<string, LogLevel, bool> _filter;
-          private string _connectionString;
-          public DBLoggerProvider(Func<string, LogLevel, bool> filter, string connectionStr)
-          {
-            _filter = filter;
-            _connectionString = connectionStr;
-          }
-          public ILogger CreateLogger(string categoryName)
-          {
-            return new DBLogger(categoryName, _filter, _connectionString);
-          }
-          public void Dispose()
-          {
-          }
-    }
+     // public class DBLoggerProvider : ILoggerProvider
+     // {
+          //private readonly Func<string, LogLevel, bool> _filter;
+          //private string _connectionString;
+          //public DBLoggerProvider(Func<string, LogLevel, bool> filter, string connectionStr)
+          //{
+          //  _filter = filter;
+          //  _connectionString = connectionStr;
+          //}
+          //public ILogger CreateLogger(string categoryName)
+          //{
+          //  return new DBLogger(categoryName, _filter, _connectionString);
+          //}
+          //public void Dispose()
+          //{
+          //}
+  //  }
 }
